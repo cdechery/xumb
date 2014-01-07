6e884b8daa9e301517710f5e1755fed7f8a29a5b
cdechery <cdechery@gmail.com>
2014-01-07 14:51:43 -0200

    New 'Reset Password' feature

--- application/views/login.php
+++ application/views/login.php
@@ -15,5 +15,21 @@
 <p>Username: <input type="text" name="login" id="login" /></p>
 <p>Password: <input type="password" name="password" id="password"/></p>
 <p><input type="submit" value="Go"/></p>
+<p><a class="various" href="<?php echo base_url()?>user/reset_password" data-fancybox-type="iframe">Forgot your password?</a></p>
 </form>
 </div>
+<script>
+$(document).ready(function() {
+	$(".various").fancybox({
+		maxWidth	: 300,
+		maxHeight	: 100,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+})
+</script>
